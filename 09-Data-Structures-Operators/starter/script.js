25430bf2eab1c4eb71b82ccd3388750af21b01e9
SethAmpofo <99514561+SethAmpofo@users.noreply.github.com>
2022-02-28 23:44:38 +0000

    The Spread Operator!

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -40,8 +40,12 @@ const restaurant = {
       `order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
 };
 
+// Destructuring Objects
 restaurant.orderDelivery({
   time: '22:30',
   address: 'Vis del Sole, 21',
@@ -81,3 +85,44 @@ const {
   fri: { open: o, close: c },
 } = openingHours;
 console.log(o, c);
+
+// The spread operator (...)
+// Without destructuring
+
+const arr = [7, 8, 9];
+const badNewAray = [1, 6, arr[0], arr[(1, arr[2])]];
+console.log(badNewAray);
+
+// With destructuring
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+// unpacks all individual elements of an array
+console.log(...arr);
+
+//Using the Spread Operator on strings
+const str = 'Jonas';
+const letters = [...str, '', 'S.'];
+console.log(letters);
+console.log(...str);
+
+// Real world example
+//using Spread operator on functions
+const ingredients = [
+  prompt("Let's make pasta! Ingredient 1?"),
+  prompt('Ingredient 2?'),
+  prompt('Ingredient 3?'),
+];
+console.log(ingredients);
+
+// Without using The Spread Operator
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// Using The Spread Operator
+restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantcopy = { ...restaurant };
+restaurantcopy.name = 'Ristorante Roma';
+console.log(restaurantName);
